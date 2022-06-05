ce48537bfde232e5593471dc0ae826ab28edd816
levintoo <tooklevin@gmail.com>
2022-06-05 22:34:34 +0300

    users seeder factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    protected $model = User::class;
     /**
      * Define the model's default state.
      *
@@ -15,8 +19,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->unique()->name(),
+            'username' => $this->faker->unique()->userName(),
             'email' => $this->faker->unique()->safeEmail(),
+            'userid' => $this->faker->unique()->uuid(),
+            'phone' => $this->faker->unique()->phoneNumber(),
+            'country' => $this->faker->country(),
+            'description' => Str::random(15),
+            'profession' => $this->faker->jobTitle(),
+            'status' => Arr::random(['1','2']),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
