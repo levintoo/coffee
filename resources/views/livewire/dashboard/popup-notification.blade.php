06ee545bc1393ecccea05dba7e67b3820f5722c0
levintoo <tooklevin@gmail.com>
2022-06-07 15:32:17 +0300

    dashboard links

--- resources/views/livewire/dashboard/popup-notification.blade.php
+++ resources/views/livewire/dashboard/popup-notification.blade.php
@@ -1,4 +1,4 @@
-<li class="" wire:click="openNoty">
+<li class="" style="cursor: pointer !important;" wire:click="openNoty">
     <div class="notification-box">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g>
