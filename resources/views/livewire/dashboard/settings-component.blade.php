9a891de70e9aa58d997e5d3f321135a784d7f96a
levintoo <tooklevin@gmail.com>
2022-06-17 17:33:02 +0300

    click image to upload image

--- resources/views/livewire/dashboard/settings-component.blade.php
+++ resources/views/livewire/dashboard/settings-component.blade.php
@@ -79,8 +79,9 @@
                                     <i class="fas fa-trash text-danger"></i>
                                     <i class="fas fa-save text-success"></i>
                                 @else
+                                <label class="col-form-label" for="image-file">
                                     <img src="{{ asset('users') }}/{{ $photo }}" alt="{{ $name }}" class="img-70 rounded-circle border-radius-lg shadow-sm" wire:click="editImage">
-                                    <label class="col-form-label" for="image-file"><i class="fas fa-pen"></i></label>
+                                   </label>
                                     <input class="form-control" id="image-file" type="file" wire:model="image" hidden="true">
 
                                 @endif
