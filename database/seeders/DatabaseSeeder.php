aeb4edc9321f94e803c0bdf95cf4a8c605682663
levintoo <tooklevin@gmail.com>
2022-06-17 17:01:10 +0300

    seed permisions first before user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            SuperAdminSeeder::class,
             RolesAndPermissionsSeeder::class,
+            SuperAdminSeeder::class,
         ]);
         
     }
